3e38706b1277f92f27bbdee4b1670d30156f27d4
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-03-23 16:51:01 +0530

    Added Created AppDbContext, Added Connection string, Created DB by running migration on 23-Mar-25.

---
+++ NZWalkRevise/Database/NZWalkDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalkRevise.Models.DomainModels;
+
+namespace NZWalkRevise.Database
+{
+    public class NZWalkDbContext: DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
