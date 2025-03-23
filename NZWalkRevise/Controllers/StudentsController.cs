2da2c77509802197b90b15f9e8867807383f22ee
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-03-23 16:17:27 +0530

    Add project files.

---
+++ NZWalkRevise/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalkRevise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+                
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            string[] students = new string[] { "John", "Jane", "Doe" };
+            return Ok(students);
+        }
+    }
+}
---
+++ NZWalkRevise/Models/DomainModels/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalkRevise.Models.DomainModels
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
