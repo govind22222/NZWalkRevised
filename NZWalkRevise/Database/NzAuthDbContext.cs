8e305d4aea05c4bc4e053c1b0805a23db3ae818a
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-04-15 23:43:21 +0530

    Seeded roles to the NzAuthDbContext and Run the migration to create the Db, Lecture-61 completed on 15-Apr-2025.

--- NZWalkRevise/Database/NzAuthDbContext.cs
+++ NZWalkRevise/Database/NzAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalkRevise.Database
@@ -9,6 +10,32 @@ namespace NZWalkRevise.Database
         public NzAuthDbContext(DbContextOptions<NzAuthDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            string roleRead = "re871153-ecae-40d8-a2c1-91616ace5805";
+            string roleWrite = "w11acb22-86f4-46cf-817c-fdbc4a39a323";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = roleRead,
+                    ConcurrencyStamp = roleRead,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+
+                },
+                new IdentityRole
+                {
+                    Id = roleWrite,
+                    ConcurrencyStamp = roleWrite,
+                    Name = "Writer",
+                    NormalizedName = "Writter".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
