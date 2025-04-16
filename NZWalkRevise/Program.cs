78b56074dbb66f67d295af00f9458ec35385715b
raghvendra-imarc <raghvendra.singh@imarcgroup.com>
2025-04-16 09:42:03 +0530

    Added Code in Program.cs to Setup Identity, Lecture-62 completed on 16-Apr-2025.

--- NZWalkRevise/Program.cs
+++ NZWalkRevise/Program.cs
@@ -6,11 +6,9 @@ using NZWalkRevise.Repositories.ServiceClass;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,10 +21,29 @@ builder.Services.AddDbContext<NZWalkDbContext>(options => options.UseSqlServer(b
 //-------Adding Authentication Db by Raghvendra
 builder.Services.AddDbContext<NzAuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NzAuthConnectString")));
 
+// Adding services to the container.
 builder.Services.AddScoped<IRegion, RegionService>();
 builder.Services.AddScoped<IWalk, WalkService>();
 //-----Added By Raghvendra to use Automapper
 builder.Services.AddAutoMapper(typeof(AutomapperClass));
+
+//-------Setup Identity By Raghvendra
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Nzwalks")
+    .AddEntityFrameworkStores<NzAuthDbContext>()
+    .AddDefaultTokenProviders();
+
+//-------Added configureation for password by Raghvendra
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
 
 //-----Added By Raghvendra to Authentication and Authorization using JWT.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
@@ -39,8 +56,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
         ValidIssuer = builder.Configuration["JWT:Issuer"],
         ValidAudience = builder.Configuration["JWT:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-    }
-    );
+    });
 
 var app = builder.Build();
 
